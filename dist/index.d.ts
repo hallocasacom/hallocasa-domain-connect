f1780c0288f6cdca66861acdc8365e7d71fe0b2e
Martin Mueller <damadden88@googlemail.com>
2025-04-20 14:53:15 +0100

    init all 2

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Domain Connect TypeScript Library
- *
- * A TypeScript implementation of the Domain Connect protocol
- * https://www.domainconnect.org/
- */
-export * from './types';
-export { DomainConnectClient } from './client';
-export * from './utils';
--- dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Domain Connect TypeScript Library
- * Types and interfaces for Domain Connect protocol
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=types.js.map
